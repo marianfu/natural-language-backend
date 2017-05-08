24e1e3bd9c19e4dc54fab02b6f71e60307948892
gabrielprc <gabrielpablorc@gmail.com>
2017-05-07 23:47:48 -0300

    Modified npm run script

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const swaggerDoc = YAML.load('./src/api.yaml');
 
 // App
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const logger = morgan('tiny');
 
 app.listen(PORT, () => {
